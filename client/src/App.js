24a4eea83ae29fd8e26086a4a8e29d512fdd8593
Ishwari Rauniyar <ishirauniyar@gmail.com>
2022-04-06 16:07:19 +0545

    protectroute

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,46 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
-// import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
 import "./assets/css/demo.css";
 import "./assets/css/style.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Post from "./pages/Post";
 import PostView from "./pages/PostView";
 import Article from "./pages/Articles";
 import Table from "./pages/Table";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/login" element={<Login />} />
+          {/* <Route
+            exact
+            path="/login"
+            element={
+              localStorage.getItem("token") ? (
+                <Navigate to={"/article"} />
+              ) : (
+                <Login />
+              )
+            }
+          /> */}
+          <Route path="/login" element={<Login />} />
           <Route exact path="/post" element={<Post />} />
           <Route exact path="/postview" element={<PostView />} />
-          <Route exact path="/article" element={<Article />} />
+          <Route
+            path="/article"
+            element={
+              localStorage.getItem("token") ? (
+                <Article />
+              ) : (
+                <Navigate to={"/login"} />
+              )
+            }
+          />
+          <Route exact path="/register" element={<Register />} />
           <Route exact path="/table" element={<Table />} />
         </Routes>
       </BrowserRouter>
