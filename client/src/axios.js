4b1def537af60af6567bf3f6dde187725bf77c85
Ishwari Rauniyar <ishirauniyar@gmail.com>
2022-04-18 16:10:25 +0545

    axios changes

--- client/src/axios.js
+++ client/src/axios.js
@@ -8,16 +8,46 @@ import { api } from "./urlConfig";
 // }
 
 const token = localStorage.getItem("token");
-axios.defaults.headers.common["Authorization"] = token;
-console.log("tok", token);
+console.log("token", token);
+function getHeaders() {
+  if (token) {
+    return {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+  }
+}
+// axios.defaults.headers.common["Authorization"] = token;
+// console.log("tok", token);
 // if (token !== null) {
 const axiosInstance = axios.create({
   baseURL: api,
-  headers: {
-    // ["x-access-token" || "authorization"]: token ? token : "",
-    Authorization: token ? `Bearer ${token}` : "",
-  },
+  // token: localStorage.getItem("token"),
+
+  ...getHeaders(),
 });
+
+console.log("axios", axiosInstance);
 // }
+
+// const url = axios.create({
+//   baseURL: api,
+// });
+
+// const axiosInstance = url.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+//     console.log("tok", token);
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+// console.log("axios", url.baseURL);
 
 export default axiosInstance;
