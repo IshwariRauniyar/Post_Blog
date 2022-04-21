3c310779f45e2132b619b2bd34d80b843d4a9bce
Ishwari Rauniyar <ishirauniyar@gmail.com>
2022-04-21 07:06:18 +0545

    logout

--- client/src/redux/actions/auth.actions.js
+++ client/src/redux/actions/auth.actions.js
@@ -5,10 +5,8 @@ import Toast from "../../components/Toast";
 export const authLogin = (user) => async (dispatch) => {
   try {
     const { data } = await axiosInstance.post("/auth/login", user);
-    console.log(data);
     if (data.success) {
       const { token, user } = data.result;
-      console.log("ndj", data.result);
       localStorage.setItem("token", token);
       localStorage.setItem("user", JSON.stringify(user));
       dispatch({
@@ -25,19 +23,36 @@ export const authLogin = (user) => async (dispatch) => {
   }
 };
 
-export const authRegister = (user) => async (dispatch) => {
+// export const authRegister = (user) => async (dispatch) => {
+//   try {
+//     const { data } = await axiosInstance.post("/auth/register", user);
+//     if (data.success == true) {
+//       dispatch({ type: Constants.REGISTER_SUCCESS, payload: data });
+//       Toast.success(data.message);
+//       window.location.href = "/login";
+//     } else {
+//       Toast.error(data.message);
+//     }
+//   } catch (error) {
+//     Toast.error("Error registering");
+//     console.log(error);
+//   }
+// };
+
+export const authLogout = (user) => async (dispatch) => {
   try {
-    const { data } = await axiosInstance.post("/auth/register", user);
-    // console.log("data", data);
-    if (data.success == true) {
-      dispatch({ type: Constants.REGISTER_SUCCESS, payload: data });
+    const { data } = await axiosInstance.get("/auth/logout");
+    if (data.success === true) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      dispatch({ type: Constants.LOGOUT_SUCCESS, payload: data });
       Toast.success(data.message);
       window.location.href = "/login";
     } else {
       Toast.error(data.message);
     }
   } catch (error) {
-    Toast.error("Error registering");
+    Toast.error("Error logging out");
     console.log(error);
   }
 };
